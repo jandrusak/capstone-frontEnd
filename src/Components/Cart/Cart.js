23c97092c2b7374e84655d57d10cf02388a83afe
jandrusak <jonathanandrusak@gmail.com>
2023-11-07 22:02:36 -0600

    push checkout button

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -9,6 +9,8 @@ function Cart() {
   const [cartItems, setCartItems] = useState([]);
   const [itemDetails, setItemDetails] = useState([]);
 
+  const checkoutUrl = "https://stripe.com/";
+
   const fetchCart = () => {
     axios
       .get("https://sourcingmagic-backend.onrender.com/Cart", {
@@ -26,7 +28,9 @@ function Cart() {
     let data = [];
     for (let item of cartItems) {
       axios
-        .get(`https://sourcingmagic-backend.onrender.com/Products/${item.product_id}`)
+        .get(
+          `https://sourcingmagic-backend.onrender.com/Products/${item.product_id}`
+        )
         .then((response) => {
           console.log(response.data);
           data = [...data, response.data];
@@ -61,18 +65,17 @@ function Cart() {
   const handleDelete = (product_id) => {
     console.log(product_id);
     axios
-      .delete(
-        `https://sourcingmagic-backend.onrender.com/Cart/${product_id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${cookies.token}`,
-          },
-        }
-      )
+      .delete(`https://sourcingmagic-backend.onrender.com/Cart/${product_id}`, {
+        headers: {
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      })
       .then((res) => {
         console.log("message here");
         console.log(res);
-        setCartItems(cartItems.filter((item)=> item.product_id !== product_id) )
+        setCartItems(
+          cartItems.filter((item) => item.product_id !== product_id)
+        );
       });
   };
 
@@ -83,10 +86,13 @@ function Cart() {
         <div className="cart-item" key={item.product_id}>
           <div className="cart-details">
             <div className="cart-delete">
-            <img src={item?.image_url} alt={item?.product_title} />
-            <button className="button" onClick={() => handleDelete(item?.product_id)}>
-              Delete
-            </button>
+              <img src={item?.image_url} alt={item?.product_title} />
+              <button
+                className="button"
+                onClick={() => handleDelete(item?.product_id)}
+              >
+                Delete
+              </button>
             </div>
             <p>{item?.product_title}</p>
           </div>
@@ -95,6 +101,13 @@ function Cart() {
       ))}
       <h4>Total ${cartTotal()}</h4>
       {/* <p>Checkout (unavailable at this time)</p> */}
+      <a
+        href={checkoutUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="checkout-button">
+        Checkout
+      </a>
     </div>
   );
 }
